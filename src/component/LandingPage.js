0af7cc93f83d920cddcfd456bbbd016b76d0ebfe
talmukund <bal.mukund015435@gmail.com>
2018-06-15 22:16:01 +0530

    added landing page

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import logo from "./logo.svg";
+import "./LandingPage.css";
 
 export default class LandingPage extends Component {
   static propTypes = {
@@ -7,6 +9,16 @@ export default class LandingPage extends Component {
   };
 
   render() {
-    return <div>This is landing Page</div>;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
